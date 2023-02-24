8012bb1cfd1aa2784c77772ea9d3c06d24bc7abb
LeJoPat <patoizeau20@gmail.com>
2023-02-24 15:04:14 +0100

    ajout d'un exemple

--- solver.py
+++ solver.py
@@ -75,6 +75,7 @@ def premier_non_rempli(grille):
 def resolution(grille):
     i,j=premier_non_rempli(grille)
     if i==-1 and j==-1:
+        print(grille)
         return True
     else:
         val=valeurs(grille,i,j)
@@ -86,3 +87,14 @@ def resolution(grille):
                 grille[i][j]=0
     grille[i][j]=0
     return False
+
+#exemple
+resolution ([[2,4,0,0,0,3,5,9,0],\
+             [6,0,3,2,5,0,0,0,0],\
+             [0,0,5,0,0,8,6,2,0],\
+             [5,0,0,0,0,2,3,0,7],\
+             [0,0,9,0,0,0,2,0,0],\
+             [8,0,1,7,0,0,0,0,5],\
+             [0,3,6,5,0,0,8,0,0],\
+             [0,0,0,0,8,1,7,0,6],\
+             [0,1,8,3,0,0,0,5,9]])
